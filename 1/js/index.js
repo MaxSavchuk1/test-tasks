9b685e6fe4a248faedfce008da7b207938c3a574
Maksym Savchuk <m.savchuk@urancompany.com>
2023-06-06 18:26:14 +0300

    change formatter settings

--- 1/js/index.js
+++ 1/js/index.js
@@ -1,76 +1,76 @@
-'use strict'
+'use strict';
 
-const messages = { data: [] }
+const messages = { data: [] };
 
 function getData () {
   fetch('https://dummyjson.com/comments')
     .then(res => res.json())
     .then(({ comments }) => {
-      messages.data = comments
+      messages.data = comments;
     })
     .then(() => renderMessages())
-    .catch(alert)
+    .catch(alert);
 }
 
 function renderMessages () {
-  const messagesList = document.querySelector('.messagesList')
+  const messagesList = document.querySelector('.messagesList');
   if (messagesList) {
-    messagesList.remove()
+    messagesList.remove();
   }
 
-  const newMessagesList = document.createElement('ul')
-  newMessagesList.addEventListener('click', clickHandler)
-  newMessagesList.classList.add('messagesList')
-  document.getElementById('root').prepend(newMessagesList)
+  const newMessagesList = document.createElement('ul');
+  newMessagesList.addEventListener('click', clickHandler);
+  newMessagesList.classList.add('messagesList');
+  document.getElementById('root').prepend(newMessagesList);
 
   const messagesCollection = messages.data.map(item =>
     createMessagesListItem(item)
-  )
-  newMessagesList.append(...messagesCollection)
+  );
+  newMessagesList.append(...messagesCollection);
 }
 
 function createMessagesListItem ({ id, body }) {
-  const message = document.createElement('li')
-  message.classList.add('message')
-  message.id = `${id}-message` // used like 'key' in React or Vue
+  const message = document.createElement('li');
+  message.classList.add('message');
+  message.id = `${id}-message`; // used like 'key' in React or Vue
 
-  message.append(createMessageBody(id, body))
-  message.append(createDeleteButton())
+  message.append(createMessageBody(id, body));
+  message.append(createDeleteButton());
 
-  return message
+  return message;
 }
 
 function createMessageBody (id, body) {
-  const div = document.createElement('div')
-  div.classList.add('messageBody')
-  div.innerText = `${id}. ${body}`
+  const div = document.createElement('div');
+  div.classList.add('messageBody');
+  div.innerText = `${id}. ${body}`;
 
-  return div
+  return div;
 }
 
 function createDeleteButton () {
-  const img = new Image()
-  img.src = './images/trash.svg'
-  img.alt = 'delete'
+  const img = new Image();
+  img.src = './images/trash.svg';
+  img.alt = 'delete';
 
-  const button = document.createElement('button')
-  button.classList.add('deleteButton')
-  button.append(img)
+  const button = document.createElement('button');
+  button.classList.add('deleteButton');
+  button.append(img);
 
-  return button
+  return button;
 }
 
 function clickHandler (e) {
-  const targetItem = e.target
+  const targetItem = e.target;
   if (targetItem.closest('.deleteButton')) {
-    const targetMessage = targetItem.closest('.message')
+    const targetMessage = targetItem.closest('.message');
     // remove from list
     messages.data = messages.data.filter(
       el => el.id !== +targetMessage.id.split('-')[0]
-    )
+    );
     //remove from DOM
-    targetMessage.remove()
+    targetMessage.remove();
   }
 }
 
-getData()
+getData();
