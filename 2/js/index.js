be09c58a7cae3659c58ce4f20cc6fea894093daa
Maksym Savchuk <m.savchuk@urancompany.com>
2023-06-15 12:14:20 +0300

    refactor second task

--- 2/js/index.js
+++ 2/js/index.js
@@ -2,6 +2,7 @@
 
 import createStopwatch from './stopwatch.js';
 import lapList from './lapList.js';
+import keyboardControl from './keyboardControl.js';
 
 const callbacksById = {};
 const setCallbacksById = (id, callbacks) => {
@@ -17,42 +18,5 @@ createStopwatch({
   setCallbacksById,
 });
 
-// below is all for keyboard control
-const allStopwatches = document.querySelectorAll('.stopwatch');
-
-let currentStopwatchId = allStopwatches[0].id;
-
-function handleMouseOver (e) {
-  const fromElement = e.relatedTarget;
-  if (fromElement && e.currentTarget.contains(fromElement)) {
-    return;
-  }
-  currentStopwatchId = e.target.id;
-}
-
-function handleMouseOut (e) {
-  const toElement = e.relatedTarget;
-  if (toElement && e.currentTarget.contains(toElement)) {
-    return;
-  }
-  currentStopwatchId = e.target.id;
-}
-
-allStopwatches.forEach(el => {
-  el.addEventListener('mouseover', handleMouseOver);
-  el.addEventListener('mouseout', handleMouseOut);
-});
-
-function handleKeyDown (e) {
-  if (Object.keys(callbacksById).length) {
-    if (e.keyCode === 83) {
-      callbacksById[currentStopwatchId].toggleState();
-    } else if (e.keyCode === 82) {
-      callbacksById[currentStopwatchId].resetState();
-    } else if (e.keyCode === 76) {
-      callbacksById[currentStopwatchId].setLap();
-    }
-  }
-}
-
-document.addEventListener('keydown', handleKeyDown);
+// enable keyboard control
+keyboardControl(callbacksById);
